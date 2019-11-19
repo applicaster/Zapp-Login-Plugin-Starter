05b3df3508d40214ac04c23423e131d8f664bcaa
oduk <oduk@scand.com>
2019-11-19 12:32:45 +0300

    [QuickBrick] Added styles to Login Screen.

--- QuickBrick/plugin/src/TraditionalLogin/Screens/LoginScreen.js
+++ QuickBrick/plugin/src/TraditionalLogin/Screens/LoginScreen.js
@@ -4,10 +4,10 @@ import {
   Text,
   StyleSheet
 } from 'react-native';
-import Layout from "../Components/Layout";
+import Layout from "../../Common/Components/Layout";
 import LoginForm from '../Components/LoginForm';
 
-function LoginScreen() {
+function LoginScreen(props) {
 
   const onLogin = () => {
     // your login code here
@@ -15,39 +15,68 @@ function LoginScreen() {
 
   return (
     <Layout>
-      <View style={styles.container}>
+      <View style={styles.loginContainer}>
         <Text style={styles.title}>
           Login Title
         </Text>
-        <View>
-          <Text style={styles.title}>
-           Main Description
-          </Text>
-          <Text>
-            Optional requirements 1
-          </Text>
-          <Text>
-            Optional requirements 1
-          </Text>
+        <View style={styles.container}>
+          <View style={styles.textContainer}>
+            <Text style={styles.mainDescription}>
+              Main Description
+            </Text>
+            <Text style={styles.optionalRequirements}>
+              Optional requirements 1
+            </Text>
+            <Text style={styles.optionalRequirements}>
+              Optional requirements 2
+            </Text>
+          </View>
+          <View style={styles.formContainer}>
+            <LoginForm onLogin={onLogin}/>
+          </View>
         </View>
-        <LoginForm onLogin={onLogin}/>
       </View>
     </Layout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  loginContainer: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    width: '100%'
   },
   title: {
     color: "#525A5C",
     fontSize: 42,
     fontWeight: 'bold',
-    marginBottom: 110
+    marginBottom: 100
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  textContainer: {
+    flex: 1,
+    paddingRight: 20,
+    alignItems: 'flex-start'
+  },
+  formContainer: {
+    flex: 1,
+    paddingTop: 20,
+    paddingLeft: 20,
+    alignItems: 'flex-end'
+  },
+  mainDescription: {
+    color: "#525A5C",
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  optionalRequirements: {
+    color: "#525A5C",
+    fontSize: 30,
+    fontWeight: 'normal',
+    marginBottom: 10
   }
 });
 
