f3877812993ae4be8a0ef23e90135d4bc52df286
Roman Karpievich <karpievich@scand.com>
2019-11-20 16:54:40 +0300

    [iOS] Add comments.

--- iOS/Content Access Plugins Starter Kit/Classes/AnalyticsStorage.swift
+++ iOS/Content Access Plugins Starter Kit/Classes/AnalyticsStorage.swift
@@ -13,7 +13,7 @@ public class AnalyticsStorage: AnalyticsStorageProtocol {
     public var trigger: Trigger = .appLaunch
     private(set) public var itemName: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? ""
     private(set) public var itemType: String = "App"
-    private(set) public var purchasesProperties: [String: PurchaseProperties] = [:]
+    private(set) public var purchasesProperties: [String: PurchaseProperties] = [:] // Logic to update this property should be implemented separately for every plugin
     
     func updateProperties(from playableItem: ZPPlayable) {
         itemName = playableItem.playableName()
