692242c3ad2e73d41a10dee426e7cebfa85bcd67
oduk <oduk@scand.com>
2019-11-27 17:56:31 +0300

    [QuickBrick] Added QRBlock Component.

---
+++ QuickBrick/plugin/src/2ndActivationScreen/Components/QRBlock.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator
+} from 'react-native';
+import QRCode from './QRCode';
+
+function QRBlock({ loading, QRCodehintStyle, qrCodeHint }) {
+  return (
+    loading
+      ? (
+        <View style={styles.loadContainer}>
+          <ActivityIndicator size="large" color="#525A5C" />
+        </View>
+      )
+      : (
+        <View style={{ alignItems: 'center' }}>
+          <QRCode url="" />
+          <Text
+            style={{ ...QRCodehintStyle, marginTop: 20 }}
+            numberOfLines={2}
+            ellipsizeMode="tail"
+          >
+            {qrCodeHint}
+          </Text>
+        </View>
+      )
+  );
+}
+
+export default QRBlock;
+
+const styles = StyleSheet.create({
+  loadContainer: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
