e2931d65e783c227d6204b87020a1b635aa7da29
oduk <oduk@scand.com>
2019-11-27 18:25:40 +0300

    [QuickBrick] Removed font family from dynamic styles.

--- QuickBrick/plugin/src/2ndActivationScreen/Screens/SignInScreen/SignInStyles.js
+++ QuickBrick/plugin/src/2ndActivationScreen/Screens/SignInScreen/SignInStyles.js
@@ -1,48 +1,41 @@
 import { getPluginData } from '../../../Common/Utils';
 
-export function createStyleSheet(screenData, configuration) {
+export default function createStyleSheet(screenData, configuration) {
   const customStyles = getPluginData(screenData, configuration);
 
   const mainTextStyle = {
     color: customStyles.main_instructions_fontcolor,
-    fontSize: Number(customStyles.main_instructions_fontsize),
-    // fontFamily: customStyles.main_instructions_font // import font families?
+    fontSize: Number(customStyles.main_instructions_fontsize)
   };
 
   const goToTextStyle = {
     color: customStyles.go_to_url_fontcolor,
-    fontSize: Number(customStyles.go_to_url_fontsize),
-    // fontFamily: customStyles.go_to_url_font // import font families?
+    fontSize: Number(customStyles.go_to_url_fontsize)
   };
 
   const activationUrlStyle = {
     color: customStyles.activation_url_fontcolor,
-    fontSize: Number(customStyles.activation_url_fontsize),
-    // fontFamily: customStyles.activation_url_font // import font families?
+    fontSize: Number(customStyles.activation_url_fontsize)
   };
 
   const codeInstructionsStyle = {
     color: customStyles.code_instructions_fontcolor,
-    fontSize: Number(customStyles.code_instructions_fontsize),
-    // fontFamily: customStyles.code_instructions_font // import font families?
+    fontSize: Number(customStyles.code_instructions_fontsize)
   };
 
   const activationCodeStyle = {
     color: customStyles.activation_code_fontcolor,
-    fontSize: Number(customStyles.activation_code_fontsize),
-    // fontFamily: customStyles.activation_code_font // import font families?
+    fontSize: Number(customStyles.activation_code_fontsize)
   };
 
   const QRCodehintStyle = {
     color: customStyles.qr_code_hint_fontcolor,
-    fontSize: Number(customStyles.qr_code_hint_fontsize),
-    // fontFamily: customStyles.qr_code_hint_font // import font families?
+    fontSize: Number(customStyles.qr_code_hint_fontsize)
   };
 
   const additionalInfoStyle = {
     color: customStyles.additional_info_fontcolor,
-    fontSize: Number(customStyles.additional_info_fontsize),
-    // fontFamily: customStyles.additional_info_font // import font families?
+    fontSize: Number(customStyles.additional_info_fontsize)
   };
 
   return {
