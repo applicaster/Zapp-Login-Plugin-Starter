08758b79e17b35f56c3eee7d3092906d6b78cbd4
oduk <oduk@scand.com>
2019-11-27 17:56:11 +0300

    [QuickBrick] Added AdditionalInfo Component.

---
+++ QuickBrick/plugin/src/2ndActivationScreen/Components/AdditionalInfo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function AdditionalInfo(props) {
+  const {
+    additionalInfo,
+    additionalInfoStyle
+  } = props;
+
+  return (
+    <View style={styles.bottomText}>
+      <Text
+        style={additionalInfoStyle}
+        numberOfLines={2}
+        ellipsizeMode="tail"
+      >
+        {additionalInfo}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottomText: {
+    maxWidth: 1110,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 150,
+    marginBottom: 100
+  }
+});
+
+export default AdditionalInfo;
