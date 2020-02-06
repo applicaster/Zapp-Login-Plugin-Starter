405e08ef8592a95c0fafa385a53d5f1b49993d1d
Enzo D'Onofrio <enzo@EnzoAppasterMBP.fritz.box>
2020-02-06 14:38:20 +0000

    Update : change the metro.config in order to have multiple plugins folders

--- QuickBrick/metro.config.js
+++ QuickBrick/metro.config.js
@@ -1,13 +1,28 @@
-const { resolve } = require("path");
+const { resolve } = require('path');
+const R = require('ramda');
+const fs = require('fs');
+
+const packages = fs.readdirSync('./plugins');
+
+const buildExtraNodeModules = (extraNodeModules, packageName) => R.assoc(
+  `@applicaster/${packageName}`,
+  resolve(__dirname, './plugins/', packageName),
+  extraNodeModules
+);
+
+const resolveLocalPackages = (packageName) => resolve(__dirname, `./plugins/${packageName}`);
 
 const config = {
-    resolver: {
-        extraNodeModules: {
-            "react-native": resolve(__dirname, "./node_modules/react-native")
-        }
-    },
-    projectRoot: resolve(__dirname, "./development-app"),
-    watchFolders: [resolve(__dirname, "./plugin"), resolve(__dirname)]
+  resolver: {
+    extraNodeModules: {
+      'react-native': resolve(__dirname, './node_modules/react-native'),
+      ...R.reduce(buildExtraNodeModules, {}, packages)
+    }
+  },
+  watchFolders: R.compose(
+    R.append(resolve(__dirname)),
+    R.map(resolveLocalPackages)
+  )(packages)
 };
 
 module.exports = config;
