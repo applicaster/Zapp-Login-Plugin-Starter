f7712c6c3e07e9302487e184e3fb6b24233a67f8
Enzo D'Onofrio <enzo@Enzo-Applicaster-MacBook-Pro.local>
2020-02-17 16:55:16 +0000

    removed console.log

--- QuickBrick/plugins/MobileLoginScreen/src/components/commons/inputField/InputField.js
+++ QuickBrick/plugins/MobileLoginScreen/src/components/commons/inputField/InputField.js
@@ -20,9 +20,6 @@ class InputField extends Component {
     if (inputValidation) {
       event.persist()
       const value = event.nativeEvent.text
-      console.log('event', event)
-      console.log('value : ', inputValidation(value), value)
-      console.log('control !!', inputValidation(value) === '')
       this.setState({isValid: inputValidation(value) === ''})
     }
   }
@@ -30,7 +27,6 @@ class InputField extends Component {
   updateCallback = event => {
     const {onUpdateValue, inputType} = this.props
     event.persist()
-    console.log('event on change', event)
     const value = event.nativeEvent.text
     onUpdateValue(value, inputType)
   }
@@ -59,7 +55,6 @@ class InputField extends Component {
     const inputColor = textColor || colors.white
     const borderBottom = borderBottomColor || 'transparent'
 
-    console.log('isValid', isValid)
     const validationColor = isValid ? color : errorColor
 
     return (
